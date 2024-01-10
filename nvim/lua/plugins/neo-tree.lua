815d79e560f7f32164461a8f207ab97ec8fd421f
PapaPandaPC <rafaeljonathan.work@gmail.com>
2024-01-11 01:24:35 +0700

    test done, clean up

---
+++ nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,19 @@
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+				show_hidden_count = true,
+				hide_dotfiles = false,
+				hide_gitignored = false,
+				never_show = {},
+			},
+		},
+	},
+}
