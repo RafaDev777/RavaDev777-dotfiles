863e0896ea9d5e8cc651a4d252d447be92e3394b
PapaPandaPC <rafaeljonathan.work@gmail.com>
2024-01-10 23:24:42 +0700

    colorscheme done

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,11 +2,28 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
-		name = "tokyonight",
-		opts = {
-			style = "moon", -- storm/night/moon
-		},
+		priority = 1000,
 		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				transparent = false,
+				terminal_colors = true,
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true },
+					functions = {},
+					variables = {},
+					sidebars = "transparent", -- dark | transparent | normal
+					floats = "transparent", -- dark | transparent | normal
+				},
+				sidebars = { "qf", "help" },
+				day_brightness = 0.3,
+				hide_inactive_statusline = false,
+				dim_inactive = true,
+				lualine_bold = false,
+			})
+
+			-- activate the colorcheme
 			vim.cmd.colorscheme("tokyonight")
 		end,
 	},
