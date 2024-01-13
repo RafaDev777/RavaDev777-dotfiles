ce4953f744d7166eef2ea69d4f4f4fad7bc35db0
PapaPandaPC <rafaeljonathan.work@gmail.com>
2024-01-13 19:36:04 +0700

    clean up

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -7,8 +7,6 @@ return {
 		end,
 		keys = {
 			{ "<C-/>", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle Terminal" },
-			{ "<leader>tt", "<cmd>ToggleTerm 2<cr>", desc = "Open Terminal" },
-			{ "<leader>t", "<cmd>ToggleTerm<cr>", desc = "Add Terminal" },
 			{ "q", "<cmd>ToggleTerm<cr>" },
 		},
 	},
