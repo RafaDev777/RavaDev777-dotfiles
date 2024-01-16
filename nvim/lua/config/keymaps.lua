c2bb349f9d6f59a1a06441f77de108f528d80783
papapanda-MBP-Fedora <rafaeljonathan.work@gmail.com>
2024-01-16 15:46:51 +0700

    terminal config done

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,17 @@
 -- Float terminal toggle
 vim.keymap.set("n", "<C-_>", function()
-	require("lazyvim.util").terminal(nil, { border = "rounded" })
-end, { desc = "Term with border" })
+	require("lazyvim.util").terminal(nil, {
+		border = "rounded",
+		style = "minimal",
+		title = "Terminal",
+		title_pos = "center",
+		margin = {
+			top = 5,
+			bottom = 5,
+			right = 5,
+			left = 5,
+		},
+	})
+end, {
+	desc = "Term with border",
+})
